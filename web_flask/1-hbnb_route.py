4a3e6c5033aafe1d753fb900f8f449e946e15116
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-05 13:34:30 -0500

    [TASK 1-2]: add correct comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""First connection using Flask"""
+"""Routing with other root"""
 from flask import Flask
 
 app = Flask(__name__)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""First connection using Flask"""
+"""add a c routing with add value in text"""
 from flask import Flask
 from markupsafe import escape
 
@@ -21,6 +21,7 @@ def hbnb():
 
 @app.route("/c/<text>")
 def c(text):
+    """Display web app root c and add text variable"""
     return f"c {escape(text.replace('_', ' '))}"
 
 
