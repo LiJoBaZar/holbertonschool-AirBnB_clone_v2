311f7afcd20ba279850a742697e8f70e7620386c
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-05 12:28:16 -0500

    [TASK 2]: use text variable for create a root with the c space and replace underscore with space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""First connection using Flask"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """Display web app homepage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Display web app root hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    return f"c {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
