90b949358ece64ab0d2530d82a53a0f35bc449ee
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-06 13:54:12 -0500

    [TASK 6]: use logic operator for different n is odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""using flask Framework for routing"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """display the homepage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """display the 2nd url page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """display custom 3rd url page"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python(text):
+    """display custom 4th url page"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """display custom 5th url page"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_templete(n):
+    """dusplay custom 6th url page"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def number_odd_or_even(n):
+    """display custom integer 7th url page"""
+    if (n % 2) != 0:
+        """is odd"""
+        n = "{} is odd".format(n)
+        return render_template('6-number_odd_or_even.html', number=n)
+    """Is even"""
+    n = "{} is even".format(n)
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
