80858c6b28b9e44f92e44ab3359359cd0e465cb3
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-06 13:40:46 -0500

    [TASK 5]: use render template for create a h1 with the content number=number

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""using flask Framework for routing"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """display the homepage"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """display the 2nd url page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """display custom 3rd url page"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python(text):
+    """display custom 4th url page"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """display custom 5th url page"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_templete(n):
+    """dusplay custom 6th url page"""
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
