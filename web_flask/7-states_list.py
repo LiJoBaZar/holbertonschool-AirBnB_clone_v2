03f1a1e0dbbf7b4833e72ecb6ff0952ceae00b58
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-07 10:15:46 -0500

    [TASK 7]: upadte function

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """use flask with storage and db storage"""
 from models import storage
-from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,14 +9,14 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def state_list():
     """display list of states"""
-    state_ = storage.all(State)
+    state_ = storage.all("State")
     return render_template("7-states_list.html", state_dict=state_)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exc):
     """close storage"""
-    return storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
