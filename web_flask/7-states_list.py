79983b6403b38f66f39ae6afa9ee2281be584965
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-07 13:04:13 -0500

    [TASK 7]: update py file with iteration in this file and not in template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,15 +9,16 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def state_list():
     """display list of states"""
-    state_ = storage.all("State")
-    return render_template("7-states_list.html", state_dict=state_)
+    state = storage.all("State")
+    state = [state for state in state.values()]
+    return render_template("7-states_list.html", state=state)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """close storage"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
